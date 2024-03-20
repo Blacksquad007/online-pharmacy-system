2e4a53439a3121f81c7fb56584cd18410a44cbab
Blacksquad007 <112714180+Blacksquad007@users.noreply.github.com>
2024-03-20 21:10:07 +0530

    navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavScrollExample from './navbar'
 
 export default function Home() {
   return (
     <div>
+    <NavScrollExample/>
       <h1>hi</h1>
     </div>
   )
